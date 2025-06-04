9d3b190febfdc053fcdf91039f97ad0a8a04e062
RM <rmlachica31@gmail.com>
2025-06-05 01:52:11 +0800

    Achievement Update

--- js/script.js
+++ js/script.js
@@ -56,3 +56,46 @@ logolink.addEventListener("click", () => {
 
     }
 });
+
+const arrowRight = document.querySelector(".achievements-box .navigation .arrow-right");
+const arrowLeft = document.querySelector(".achievements-box .navigation .arrow-left");
+
+let index = 0;
+
+const activeAchievements = () => {
+    const imgSlide = document.querySelector('.achievements-carousel .img-slide');
+    const achievementsDetails = document.querySelectorAll('.achievements-detail');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    achievementsDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    achievementsDetails[index].classList.add('active');
+}
+
+arrowRight.addEventListener('click', () => {
+    if(index < 3) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else{
+        index = 4;
+        arrowRight.classList.add('disabled');
+    }
+
+    activeAchievements();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if(index > 1) {
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else{
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activeAchievements();
+});
